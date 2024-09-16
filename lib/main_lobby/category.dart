2735964297560b9e890859e6e6e9b6cf50ca4128
JoHyeongGu <whgudrn123@naver.com>
2024-09-16 22:27:53 +0900

    Clean the Animated Category List

--- lib/main_lobby/category.dart
+++ lib/main_lobby/category.dart
@@ -1,152 +1,101 @@
 import 'package:flutter/material.dart';
 
 class Category extends StatefulWidget {
-  const Category(this.categories, {super.key, required this.width});
+  const Category(this.categories, {super.key, this.outPadding = 0});
   final List<Map> categories;
-  final double width;
+  final double outPadding;
 
   @override
   State<Category> createState() => _CategoryState();
 }
 
 class _CategoryState extends State<Category> {
-  late ScrollController _scrollController;
-  late double startOffset;
-  late double endOffset;
-  late double stepOffset;
-  late double prevStartOffset;
+  double gap = 40;
   double tileWidth = 300;
-  double rightPadding = 40;
-  int focus = 1;
+  double tileHeight = 400;
+  double innerPadding = 0;
+  int screenCount = 0;
+  late int focus;
 
-  double getCloseDes() {
-    double getDistance(double des) {
-      double distance = des - _scrollController.offset;
-      if (distance < 0) distance = -distance;
-      return distance;
-    }
-
-    List<double> desList = [
-      startOffset + ((focus - 1) * stepOffset),
-      startOffset + (focus * stepOffset),
-      startOffset + ((focus + 1) * stepOffset),
-    ];
-    double closeDis = getDistance(desList[0]);
-    double closeDes = desList[0];
-    int selectedIndex = 0;
-    for (int i = 0; i < desList.length; i++) {
-      double dis = getDistance(desList[i]);
-      if (dis <= closeDis) {
-        closeDis = dis;
-        closeDes = desList[i];
-        selectedIndex = i;
-      }
-    }
-    focus = focus + selectedIndex + 1;
-    if (focus >= widget.categories.length) focus = 0;
-    return closeDes;
+  void initWithSize() {
+    screenCount = (MediaQuery.of(context).size.width / tileWidth).toInt();
+    if (screenCount % 2 == 0) screenCount--;
+    innerPadding = MediaQuery.of(context).size.width / 10;
   }
 
   void autoScroll() async {
-    await Future.delayed(const Duration(seconds: 1));
     while (true) {
-      if (focus >= widget.categories.length) focus = 0;
+      await Future.delayed(Duration(seconds: 3));
       focus++;
-      setOffset();
-      if (prevStartOffset != startOffset) {
-        _scrollController.animateTo(
-          getCloseDes(),
-          duration: const Duration(milliseconds: 800),
-          curve: Curves.linear,
-        );
-        prevStartOffset = startOffset;
-        await Future.delayed(const Duration(seconds: 1));
-      } else if (_scrollController.offset >= endOffset) {
-        _scrollController.jumpTo(startOffset);
-        await Future.delayed(const Duration(milliseconds: 100));
-      }
-      _scrollController.animateTo(
-        _scrollController.offset + tileWidth + rightPadding,
-        duration: const Duration(milliseconds: 800),
-        curve: Curves.easeInOut,
-      );
-      await Future.delayed(const Duration(seconds: 2));
+      if (focus >= widget.categories.length) focus = 0;
+      setState(() {});
     }
   }
 
-  void setOffset() {
-    stepOffset = tileWidth + rightPadding;
-    late double tileCount;
-    try {
-      tileCount = MediaQuery.of(context).size.width / tileWidth;
-    } catch (e) {
-      tileCount = widget.width / tileWidth;
-    }
-    double backTo = tileWidth * ((tileCount - 1) / 2);
-    startOffset = (stepOffset * widget.categories.length) - backTo;
-    endOffset = startOffset + (stepOffset * (widget.categories.length));
-    // try {
-    //   _scrollController.animateTo(
-    //     startOffset,
-    //     duration: const Duration(milliseconds: 300),
-    //     curve: Curves.easeInOut,
-    //   );
-    // } catch (e) {}
+  double getCenterPos() {
+    return (MediaQuery.of(context).size.width / 2) -
+        (tileWidth / 2) -
+        (widget.outPadding + innerPadding);
+  }
+
+  Widget animatedTile(Map cate) {
+    double centerPos = getCenterPos();
+    double pos = screenCount == 0
+        ? centerPos
+        : centerPos + (tileWidth + gap) * (cate["index"] - focus);
+    return AnimatedPositioned(
+      curve: Curves.easeInBack,
+      duration: Duration(milliseconds: 700 + (cate["index"] * 40).toInt() as int),
+      left: pos,
+      child: CategoryTile(
+        cate,
+        width: tileWidth,
+        height: tileHeight,
+      ),
+    );
+  }
+
+  Widget animatedCategories() {
+    return AnimatedPadding(
+      duration: const Duration(milliseconds: 300),
+      padding: EdgeInsets.symmetric(horizontal: innerPadding),
+      child: Stack(
+        children: widget.categories.map((cate) => animatedTile(cate)).toList(),
+      ),
+    );
   }
 
   @override
   void initState() {
     super.initState();
-    setOffset();
-    prevStartOffset = startOffset;
-    _scrollController = ScrollController(initialScrollOffset: startOffset);
+    if (widget.categories.length % 2 == 0) {
+      widget.categories.add({});
+    }
+    for (MapEntry cate in widget.categories.asMap().entries) {
+      cate.value["index"] = cate.key;
+    }
+    focus = widget.categories.length ~/ 2;
     autoScroll();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        IconButton(
-          onPressed: () {
-            setOffset();
-          },
-          icon: Icon(Icons.refresh),
-        ),
-        Container(
-          margin: const EdgeInsets.only(top: 15),
-          child: SingleChildScrollView(
-            controller: _scrollController,
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                ...widget.categories.map(
-                  (cate) => CategoryTile(cate,
-                      width: tileWidth, rightPadding: rightPadding),
-                ),
-                ...widget.categories.map(
-                  (cate) => CategoryTile(cate,
-                      width: tileWidth, rightPadding: rightPadding),
-                ),
-                ...widget.categories.map(
-                  (cate) => CategoryTile(cate,
-                      width: tileWidth, rightPadding: rightPadding),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
+    initWithSize();
+    return SizedBox(
+      width: double.infinity,
+      height: tileHeight,
+      child: animatedCategories(),
     );
   }
 }
 
 class CategoryTile extends StatefulWidget {
   const CategoryTile(this.data,
-      {super.key, required this.width, required this.rightPadding});
+      {super.key, this.width = 300, this.height = 400, this.detail});
   final Map data;
+  final bool? detail;
   final double width;
-  final double rightPadding;
+  final double height;
 
   @override
   State<CategoryTile> createState() => _CategoryTileState();
@@ -173,9 +122,8 @@ class _CategoryTileState extends State<CategoryTile> {
           print(widget.data["path"]);
         },
         child: Container(
-          margin: EdgeInsets.only(right: widget.rightPadding),
           width: widget.width,
-          height: MediaQuery.of(context).size.height / 3,
+          height: widget.height,
           decoration: BoxDecoration(
             color: Colors.grey,
             borderRadius: BorderRadius.circular(15),
@@ -191,8 +139,12 @@ class _CategoryTileState extends State<CategoryTile> {
               height: widget.width,
               child: Column(
                 children: [
-                  Text(widget.data["title"]),
-                  Text(widget.data["description"]),
+                  Text(widget.data.keys.toList().contains("title")
+                      ? widget.data["title"]
+                      : ""),
+                  Text(widget.data.keys.toList().contains("description")
+                      ? widget.data["description"]
+                      : ""),
                 ],
               ),
             ),
--- lib/main_lobby/main_frame.dart
+++ lib/main_lobby/main_frame.dart
@@ -12,6 +12,11 @@ class MainFrame extends StatefulWidget {
 class _MainFrameState extends State<MainFrame> {
   final List<Map> categories = [
     {
+      "title": "0테스트 카테고리",
+      "description": "카테고리가 화면에 보이는 부분을 테스트하기 위한 테스트 데이터 입니다.",
+      "path": "/test/",
+    },
+    {
       "title": "1테스트 카테고리",
       "description": "카테고리가 화면에 보이는 부분을 테스트하기 위한 테스트 데이터 입니다.",
       "path": "/test/",
@@ -36,32 +41,49 @@ class _MainFrameState extends State<MainFrame> {
       "description": "카테고리가 화면에 보이는 부분을 테스트하기 위한 테스트 데이터 입니다.",
       "path": "/test/",
     },
-    {
-      "title": "6테스트 카테고리",
-      "description": "카테고리가 화면에 보이는 부분을 테스트하기 위한 테스트 데이터 입니다.",
-      "path": "/test/",
-    },
-    {
-      "title": "7테스트 카테고리",
-      "description": "카테고리가 화면에 보이는 부분을 테스트하기 위한 테스트 데이터 입니다.",
-      "path": "/test/",
-    },
-    {
-      "title": "8테스트 카테고리",
-      "description": "카테고리가 화면에 보이는 부분을 테스트하기 위한 테스트 데이터 입니다.",
-      "path": "/test/",
-    },
   ];
+  double contentPadding = 10;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(10),
+      padding: EdgeInsets.all(contentPadding),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           const TitleLogo(),
-          Category(categories, width: MediaQuery.of(context).size.width),
+          const SizedBox(height: 30),
+          Category(categories, outPadding: contentPadding),
+          const PostList(),
+        ],
+      ),
+    );
+  }
+}
+
+class PostList extends StatelessWidget {
+  const PostList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 40),
+      child: Column(
+        children: [
+          Container(
+            width: 300,
+            height: 40,
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              borderRadius: BorderRadius.circular(10),
+              border: const Border(
+                top: BorderSide(color: Colors.black, width: 2),
+                left: BorderSide(color: Colors.black, width: 2),
+                right: BorderSide(color: Colors.black, width: 2),
+                bottom: BorderSide(color: Colors.black, width: 2),
+              ),
+            ),
+          )
         ],
       ),
     );
